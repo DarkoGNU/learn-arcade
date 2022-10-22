c35f68059c24c5a5bd2ae5698dece66297fa1b9a
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2022-10-22 21:08:18 +0200

    Some printing

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-# Test
+print("Hello from the world of Python.")
+print("I am a very boring person, so Python is more interesting than me.")
+print("By the way, I love using quotes: \"\"\"")
+print("And\nthat's\nall")
